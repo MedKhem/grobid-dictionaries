b62469d09544e6c03f343033ddc85b76f687d450
Luca Foppiano <luca@foppiano.org>
2016-08-05 14:48:05 +0200

    #1 Extract and adapt classes for the core package: Using the utility methods provided by the core, to create/delete temporary files

--- src/main/java/org/grobid/service/DictionaryProcessFile.java
+++ src/main/java/org/grobid/service/DictionaryProcessFile.java
@@ -8,14 +8,13 @@ import org.grobid.core.engines.EngineParsers;
 import org.grobid.core.engines.LexicalEntriesParser;
 import org.grobid.core.engines.SegmentationLabel;
 import org.grobid.core.engines.config.GrobidAnalysisConfig;
-import org.grobid.core.exceptions.GrobidException;
-import org.grobid.core.utilities.GrobidProperties;
-import org.grobid.core.utilities.Utilities;
+import org.grobid.core.utilities.IOUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.core.Response;
-import java.io.*;
+import java.io.File;
+import java.io.InputStream;
 import java.util.NoSuchElementException;
 import java.util.SortedSet;
 import java.util.function.Consumer;
@@ -33,7 +32,6 @@ public class DictionaryProcessFile {
      * @return a response object mainly contain the TEI representation of the
      * full text
      */
-
     public static Response processLexicalEntries(final InputStream inputStream) {
         LOGGER.debug(methodLogIn());
         Response response = null;
@@ -47,13 +45,12 @@ public class DictionaryProcessFile {
          /*
             PDF -> [pdf2xml] -> XML -> [GROBID Segmenter model] ->  Segmented document -> [LexicalEntriesParser] -> List<LexicalEntries>
          */
-
         try {
             LOGGER.debug(">> set raw text for stateless quantity service'...");
             long start = System.currentTimeMillis();
 
             // Does GrobidRestUtils need to be imported ?
-            originFile = writeInputFile(inputStream);
+            originFile = IOUtilities.writeInputFile(inputStream);
 
             if (originFile == null) {
                 response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
@@ -98,7 +95,7 @@ public class DictionaryProcessFile {
             LOGGER.error("An unexpected exception occurs. ", e);
             response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.getCause().getMessage()).build();
         } finally {
-            removeTempFile(originFile);
+            IOUtilities.removeTempFile(originFile);
         }
         LOGGER.debug(methodLogOut());
         return response;
@@ -112,64 +109,4 @@ public class DictionaryProcessFile {
         return "<< " + DictionaryProcessFile.class.getName() + "." + Thread.currentThread().getStackTrace()[1].getMethodName();
     }
 
-
-    //To be moved somewhere generic where can be used.
-    @Deprecated
-    public static File writeInputFile(InputStream inputStream) {
-        LOGGER.debug(">> set origin document for stateless service'...");
-
-        File originFile = null;
-        OutputStream out = null;
-        try {
-            originFile = newTempFile("origin", ".pdf");
-
-            out = new FileOutputStream(originFile);
-
-            byte buf[] = new byte[1024];
-            int len;
-            while ((len = inputStream.read(buf)) > 0) {
-                out.write(buf, 0, len);
-            }
-        } catch (IOException e) {
-            LOGGER.error(
-                    "An internal error occurs, while writing to disk (file to write '"
-                            + originFile + "').", e);
-            originFile = null;
-        } finally {
-            try {
-                if (out != null) {
-                    out.close();
-                }
-                inputStream.close();
-            } catch (IOException e) {
-                LOGGER.error("An internal error occurs, while writing to disk (file to write '"
-                        + originFile + "').");
-                originFile = null;
-            }
-        }
-        return originFile;
-    }
-
-    @Deprecated
-    public static File newTempFile(String fileName, String extension) {
-        try {
-            return File.createTempFile(fileName, extension, GrobidProperties.getTempPath());
-        } catch (IOException e) {
-            throw new GrobidException(
-                    "Could not create temprorary file, '" + fileName + "." +
-                            extension + "' under path '" + GrobidProperties.getTempPath() + "'.");
-        }
-    }
-
-    @Deprecated
-    public static void removeTempFile(final File file) {
-        try {
-            // sanity cleaning
-            Utilities.deleteOldies(GrobidProperties.getTempPath(), 300);
-            LOGGER.debug("Removing " + file.getAbsolutePath());
-            file.delete();
-        } catch (Exception exp) {
-            LOGGER.error("Error while deleting the temporary file: " + exp);
-        }
-    }
 }
