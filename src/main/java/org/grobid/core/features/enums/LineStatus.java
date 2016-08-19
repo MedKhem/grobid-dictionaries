387e3c6261b656b1e069860869a98e17d8859335
Luca Foppiano <luca@foppiano.org>
2016-08-19 16:01:13 +0200

    Adding fromName in the LineStatus enumerator in order to get enumerator from String input

--- src/main/java/org/grobid/core/features/enums/LineStatus.java
+++ src/main/java/org/grobid/core/features/enums/LineStatus.java
@@ -1,19 +1,32 @@
 package org.grobid.core.features.enums;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 /**
  * Created by med on 17.08.16.
  */
 public enum LineStatus {
-
-
-    LINESTART("LINESTART"),
-    LINEIN("LINEIN"),
-    LINEEND("LINEEND");
+    LINE_START("LINESTART"),
+    LINE_IN("LINEIN"),
+    LINE_END("LINEEND");
 
     private final String name;
 
-    private LineStatus(String s) {
+    LineStatus(String s) {
         name = s;
+    }
+
+    public static LineStatus fromName(String name) {
+        final List<LineStatus> first = Arrays.stream(LineStatus.values()).filter(lineStatus -> {
+            return StringUtils.equals(lineStatus.toString(), name);
+        }).collect(toList());
+
+        return first.size() > 0 ? first.get(0) : null;
     }
 
     public boolean equalsName(String otherName) {
