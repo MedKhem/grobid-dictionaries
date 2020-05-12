0dec9ce74f3e55ae3bbbfe12c89cd81425114f92
Mohamed Khemakhem <medkhemakhemfsegs@gmail.com>
2020-05-12 16:29:29 +0200

    start integrating alto

---
+++ src/test/java/org/grobid/core/engines/LexicalEntryPDFALTOSAXParser.java
@@ -0,0 +1,60 @@
+package org.grobid.core.engines;
+
+import org.grobid.core.document.Document;
+import org.grobid.core.document.DocumentSource;
+import org.grobid.core.layout.GraphicObject;
+import org.grobid.core.layout.LayoutToken;
+import org.grobid.core.sax.PDFALTOSaxHandler;
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.CoreMatchers.is;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.easymock.EasyMock.createMock;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Med on 12.05.20.
+ */
+public class LexicalEntryPDFALTOSAXParser {
+    SAXParserFactory spf = SAXParserFactory.newInstance();
+    PDFALTOSaxHandler target;
+    DocumentSource mockDocumentSource;
+    Document document;
+    private List<GraphicObject> images;
+
+    @Before
+    public void setUp() throws Exception {
+
+        mockDocumentSource = createMock(DocumentSource.class);
+
+        document = Document.createFromText("");
+        images = new ArrayList<>();
+        target = new PDFALTOSaxHandler(document, images);
+    }
+
+    @Test
+    public void testParsing_pdf2XMLwithNoIMages_ShouldWork() throws Exception {
+        InputStream is = this.getClass().getResourceAsStream("/pdfalto_noImages.xml");
+
+        SAXParser p = spf.newSAXParser();
+        p.parse(is, target);
+
+        List<LayoutToken> tokenList = target.getTokenization();
+        System.out.print(tokenList.toString());
+        assertTrue(tokenList.size() > 0);
+        assertTrue(document.getImages().size() == 0);
+        assertTrue(images.size() == 0);
+        assertTrue(document.getPages().size() == 4);
+        assertTrue(document.getBlocks().size() == 26);
+    }
+
+
+
+
+}
